55bde10dfc29e5aea5f94ae7398f7688c2198513
ansohui <2022112701@dgu.ac.kr>
2025-11-21 12:56:59 +0900

    log 엔티티

---
+++ src/main/java/com/db/bank/domain/entity/Log.java
@@ -0,0 +1,54 @@
+package com.db.bank.domain.entity;
+import com.db.bank.domain.enums.log.Action;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "log")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Log {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "log_id")
+    private Long id;
+
+    //==Fk==
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "transaction_id",nullable = false)
+    private Transaction transaction;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_num", nullable = false)
+    private Account account;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "actor_user_id", nullable = false)
+    private User actorUser;
+
+    // ===== 잔액 정보 =====
+
+    @Column(name = "before_balance", nullable = false, precision = 18, scale = 2)
+    private BigDecimal beforeBalance;
+
+    @Column(name = "after_balance", nullable = false, precision = 18, scale = 2)
+    private BigDecimal afterBalance;
+
+    // 입금 / 출금 / 불법 / 수정
+    @Enumerated(EnumType.STRING)
+    @Column(name = "action", nullable = false, length = 10)
+    private Action action;
+
+    // 로그 생성 시간
+    @Column(name = "created_at", nullable = false,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/db/bank/domain/enums/log/Action.java
@@ -0,0 +1,9 @@
+package com.db.bank.domain.enums.log;
+
+public enum Action {
+    DEPOSIT,        // 입금
+    WITHDRAW,       // 출금
+    FRAUD,          // 불법/이상거래
+    ADJUST          // 수정
+
+}
